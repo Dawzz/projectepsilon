83a874a539ad2a0432e99fa2af0a0d57775a1893
Brenn10 <brenn.cain@gmail.com>
2015-10-05 23:39:51 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,41 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<!--Designed by Neon 2015--> 
-<head>
-<meta charset="utf-8">
- <link href="http://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
- <meta name="description" content="A new way to carpool.">
-	<title>Project Epsilon</title>
-  <link rel="stylesheet" href="index.css">
-  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-  <link rel="icon" href="/favicon.ico" type="image/x-icon">
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  <script type="text/javascript" src="navbar.js"></script>
-</head>
-<body>
-<header>
-<div class="header">
-	<img src="images/logo.png" alt="Project Epsilon Logo" id="logo" width="75" height="75"></img>
-	<a href="login.php" id="login">Log In</a>
-</div>
-<div id="navbar">
-	<div id="navSpacer"><br></div>
-        <div id="navWrap">
-            	<div id="nav">
-					<div class="center">
-		                <ul id="navbarul" style="height: 51px;">
-		                    	<li class="navbaritem"><a href="index.php" class="smoothScroll">Home</a></li>
-		                    	<li class="navbaritem"><a href="news.php" class="smoothScroll">News</a></li>
-								<li class="navbaritem"><a href="about.php" class="smoothScroll">About</a></li>
-								<li class="navbaritem"><a href="contact.php" class="smoothScroll">Contact Us</a></li>
-		                </ul>    
-	                	<br class="clearLeft" />
-            		</div>
-				</div>
-    	</div>
-	<br>
-</div>
-</header>
+<?php
+	include_once 'header.php';
+?>
 <content>
 <?php
 
@@ -460,7 +425,7 @@ $application = new OneFileLoginApplication();
 <footer>
 	<br><br>
 	<hr>
-	Content Copyright 2015
+	Content Copyright  <?php echo date("Y");?>
 </footer>
 </body>
 </html>
