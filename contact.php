ea42bffa301e277bb8e4421bf1de3d4da1b328f5
NeonRD1 <isaiahho@live.com>
2015-10-06 15:10:43 -0400

    Update email validation script
    Changed regex system

--- contact.php
+++ contact.php
@@ -7,22 +7,22 @@
 <h2>Want to contact us?</h2> 
 <p>You can email us at projectepsilon12@outlook.com or use the following form to send us a message.</p>
 <div id="contactform">
-<form action="contactform.php" method="post">
+<form> <!--action="contactform.php" method="post"-->
 	First Name:<br>
 	<input type="text" name="firstname">
 	<br>
 	Email:<br>
-	<input type="text" name="email">
+	<input type="text" name="email" id="email">
 	<br>
 	<br>
-	<input type="text" name="message">
+	<textarea cols="75" rows="6" name="message"></textarea>
 	<br>
 	<br>
 	<input type="submit" value="Submit" id="formsubmit">
 </form>	
-<script type="javascript" src="updatejs.js"></script>
 </div>
 </div>
 </content>
+<script type="text/javascript" src="updatejs.js"></script>
 </body>
 </html>
--- updatejs.js
+++ updatejs.js
@@ -3,16 +3,23 @@ emailsubmit.onclick = function() {
 	var emailinput = document.getElementById("email").value;
 	var position = emailinput.indexOf("@");
 	var position2 = emailinput.indexOf(".");
+	function validateEmail(emailinput) {
+		var re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+		return re.test(emailinput);
+	}
+		
 	if (emailinput.length <= 2) {
 		window.alert("That is not a valid email!");
+		return false;
 	}
 	else {
-		if (position == -1 || position2 == -1) {
-			window.alert("That is not a valid email!");
+		
+		if (validateEmail(emailinput)) {
+			window.alert("Thank you for your email!");
 		}
 		else {
-			window.alert("Thank you for your email!");
-			}
+			window.alert("That is not a valid email!");
+			return false;
 		}
 	}
 }
